1a30ba64e319866034e0d0eda8586216bc7281bd
Abner Matheus <abnerm80@gmail.com>
2020-04-19 15:57:37 -0300

    Arrumando o redirecionamento entre páginas

--- BackEnd/src/Controller/VerificacaoController.js
+++ BackEnd/src/Controller/VerificacaoController.js
@@ -8,7 +8,7 @@ module.exports ={
         const {cod} = request.query // http://localhost:3333/valida?cod=(código de verificacao)
         
         await connection('usuario').where('cod','=',cod).update({verificado:1})
-        response.redirect('http://localhost/MOJA/page/verificacao.html') // Redirecionar para uma página de dizendo que foi vereficado (FRONT)
+        response.redirect('http://localhost/ServicoStream/page/verificacao.html') // Redirecionar para uma página de dizendo que foi vereficado (FRONT)
         
     }, 
 
