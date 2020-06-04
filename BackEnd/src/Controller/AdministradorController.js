f72f77dc27158444793b4e5b258a87055163793d
Abner Matheus <abnerm80@gmail.com>
2020-06-04 02:21:56 -0300

    Ajustes Adm

--- BackEnd/src/Controller/AdministradorController.js
+++ BackEnd/src/Controller/AdministradorController.js
@@ -3,17 +3,17 @@ const connection = require('../database/connection');
 module.exports = {
     async valida (request,response){
         const {token}  = request.query;
-        console.log("TOKEN"+token);
+        
 
         
         try {
             
            // const [email_cad] = await connection('usuario').select('email').where('email',email);
-            const [idUser] = await connection('usuario').select('idusuario').where('cod','db343e32fb');
+            const [idUser] = await connection('usuario').select('idusuario').where('cod',String(token));
             console.log('ID USER'+idUser.idusuario);
             if(!idUser){
 
-                response.redirect('https://mojal.netlify.app');
+                response.json({status:false});
 
             }else if(idUser.idusuario == 1){
                 
@@ -21,7 +21,7 @@ module.exports = {
             }
         } catch (error) {
             console.log('Erro:'+error);
-            response.redirect('https://mojal.netlify.app');
+            response.json({status:false});
         }
         
     }
