fb1f69062ecfbbc26cc2458e4e67719dd277aa92
osvaldocabral <osvaldo_cabral@outlook.com.br>
2020-06-04 01:34:34 -0300

    gerenciador de filmes

--- script/gerenciador_filmeseries.js
+++ script/gerenciador_filmeseries.js
@@ -53,7 +53,8 @@ var gerenciar_filmeSeries = {
         let duracao = $('#duracao_input').val();
         let tipo = $('#tipo_input').val();
         let descricao = $('#descricao_input').val();
-        let cartaz = $("#cartaz_input")[0].style['background-image'].slice(4, -1);
+        //let cartaz = $("#cartaz_input")[0].style['background-image'].slice(4, -1);
+        let cartaz = $("#cartaz").val();
         
         let data= {};
         data.tipo = tipo;
@@ -96,7 +97,8 @@ var gerenciar_filmeSeries = {
         let duracao = $('#duracao_input').val();
         let tipo = $('#tipo_input').val();
         let descricao = $('#descricao_input').val();
-        let cartaz = $("#cartaz_input")[0].style['background-image'].slice(4, -1);
+        //let cartaz = $("#cartaz_input")[0].style['background-image'].slice(4, -1);
+        let cartaz = $("#cartaz").val();
         
         let data= {};
         data.id = id;
@@ -208,7 +210,8 @@ var gerenciar_filmeSeries = {
         $('#ano_input').val("");
         $('#duracao_input').val("");
         $('#descricao_input').val("");
-        $("#cartaz_input")[0].style['background-image'] = "";
+        //$("#cartaz_input")[0].style['background-image'] = "";
+        $("#cartaz").val("");
     },
 
     prepareUpdate: function(elementTr) {
