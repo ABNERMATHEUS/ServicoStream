71f8a37657707eb3b6d7973758b855fb29ef150f
Abner Matheus <abnerm80@gmail.com>
2020-06-04 17:30:07 -0300

    ajustes final

--- BackEnd/src/Controller/AdministradorController.js
+++ BackEnd/src/Controller/AdministradorController.js
@@ -3,14 +3,14 @@ const jwt = require('jsonwebtoken');
 
 module.exports = {
     async valida (request,response){
-        const {token,id}  = request.query;
+        const {token}  = request.query;
         
         const cod = String(token);
         
 
         
         try {
-            const {id} =  await jwt.verify(id,'chaveadm');
+             // await jwt.verify(id,'chaveadm');
            // const [email_cad] = await connection('usuario').select('email').where('email',email);
             const [idUser] = await connection('usuario').select('idusuario').where('cod',cod);
             
