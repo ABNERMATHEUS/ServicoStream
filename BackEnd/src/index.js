340673feb0397082a46461e2d677be832bc8a615
Abner Matheus <abnerm80@gmail.com>
2020-06-03 22:37:24 -0300

    Teste

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -4,9 +4,15 @@ const cors = require('cors')
 const bodyParser = require("body-parser")
 
 const app = express();
+
+
+app.use(cors());
+app.use(cors({ origin: 'https://mojal.netlify.app' }))
+
 app.use((req,res,next)=>{
+    
     res.setHeader('Access-Control-Allow-Originl','*');
-    app.use(cors());
+    
     next();
    
 })
