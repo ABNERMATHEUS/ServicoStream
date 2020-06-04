ecf2723219d803b085a62880f36064a7ec86f21f
Abner Matheus <abnerm80@gmail.com>
2020-06-04 02:27:07 -0300

    Ajustes

--- BackEnd/src/Controller/AdministradorController.js
+++ BackEnd/src/Controller/AdministradorController.js
@@ -4,12 +4,13 @@ module.exports = {
     async valida (request,response){
         const {token}  = request.query;
         
-
+        const cod = String(token);
         
         try {
             
            // const [email_cad] = await connection('usuario').select('email').where('email',email);
-            const [idUser] = await connection('usuario').select('idusuario').where('cod',String(token));
+            const [idUser] = await connection('usuario').select('idusuario').where('cod',cod);
+            
             console.log('ID USER'+idUser.idusuario);
             if(!idUser){
 
