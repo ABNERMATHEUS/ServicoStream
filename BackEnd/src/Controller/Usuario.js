67269c8771e741cdf9cee9e69c37b61ad01698af
Abner Matheus <abnerm80@gmail.com>
2020-06-04 01:33:16 -0300

    Ajustes

--- BackEnd/src/Controller/Usuario.js
+++ BackEnd/src/Controller/Usuario.js
@@ -94,7 +94,7 @@ module.exports = {
             //const bool = await connection('usuario').where('cod','=',token).update({senha:senha});
             const a = await connection('usuario').where('idusuario','=',idUser.idusuario).update({cod:cod});
             console.log("BANCO="+a);
-            response.redirect('https://mojal.netlify.app/page/gerenciadordefilmes?token='+cod);
+            response.json({status:1,cod:cod});
         }
         else {   
             
